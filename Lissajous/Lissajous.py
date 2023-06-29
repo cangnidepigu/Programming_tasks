4ceeb0a87de1fd5a76010bb696f8d5c9c5d40ac5
cangnidepigu <finalandeventual@gmail.com>
2023-06-29 18:17:33 +0200

    Improvements and add dynamic params changing

--- Lissajous/Lissajous.py
+++ Lissajous/Lissajous.py
@@ -4,24 +4,25 @@ import math
 width = 800
 height = 600
 
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Lissajous Curve")
-clock = pygame.time.Clock()
 
-
-def draw_point_for_lissajous_curve(a, b, delta, time):
+def draw_point(a, b, delta, time, screen, clock, line_surface):
     screen.fill((255, 255, 255))
 
     # Calculate the current position on the Lissajous curve
     x = width / 2 + (width / 2 - 50) * math.sin(a * time)
     y = height / 2 + (height / 2 - 50) * math.sin(b * time + delta)
 
+    pygame.draw.line(line_surface, (255, 255, 255), (int(x), int(y)), (int(x), int(y)))
+
+    screen.blit(line_surface, (0, 0))
+
     # Draw the point
     pygame.draw.circle(screen, (0, 0, 0), (int(x), int(y)), 5)
 
     pygame.display.flip()
     clock.tick(60)
+
+    return x, y
 
 
 def main():
@@ -33,6 +34,14 @@ def main():
         input("Enter the value of 'δ' (delta) (phase difference between x and y): ")
     )
 
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Lissajous Curve")
+    clock = pygame.time.Clock()
+
+    # Create a separate surface for drawing the line segments
+    line_surface = pygame.Surface((width, height))
+
     time = 0
 
     running = True
@@ -42,11 +51,24 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        draw_point_for_lissajous_curve(a, b, delta, time)
+            # Check for key presses
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    a = float(input("Enter the new value of 'a': "))
+                elif event.key == pygame.K_b:
+                    b = float(input("Enter the new value of 'b': "))
+                elif event.key == pygame.K_d:
+                    delta = float(input("Enter the new value of 'δ' (delta): "))
+                elif event.key == pygame.K_r:
+                    screen.fill((255, 255, 255))
+                    line_surface.fill((0, 0, 0))
+                elif event.key == pygame.K_e:
+                    pygame.quit()
+
+        draw_point(a, b, delta, time, screen, clock, line_surface)
 
         time += 0.01
 
-    # Quit the application
     pygame.quit()
 
 
